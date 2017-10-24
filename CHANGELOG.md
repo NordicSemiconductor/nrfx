bd4c9a10998e4d54b379b40ffd34b660ccb33ed2
Karol Lasończyk <karol.lasonczyk@nordicsemi.no>
2017-10-24 11:07:28 +0200

    nrfx 0.7.0 release

---
+++ CHANGELOG.md
@@ -0,0 +1,18 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [0.7.0] - 2017-10-20
+### Added
+- This CHANGELOG.md file.
+- README.md file with simple description and explanations.
+- HAL for: ADC, CLOCK, COMP, ECB, EGU, GPIO, GPIOTE, I2S, LPCOMP, NVMC, PDM, POWER, PPI, PWM, QDEC, QSPI, RNG, RTC, SAADC, SPI, SPIM, SPIS, ARM(R) SysTick, TEMP, TIMER, TWI, TWIM, TWIS, UART, UARTE, USBD, WDT.
+- Drivers for: ADC, CLOCK, COMP, GPIOTE, I2S, LPCOMP, PDM, POWER, PWM, QDEC, QSPI, RNG, RTC, SAADC, SPI, SPIM, SPIS, ARM(R) SysTick, TIMER, TWI, TWIM, TWIS, UART, UARTE, WDT.
+- Allocators for: PPI, SWI/EGU.
+- MDK in version 8.14.0.
+- Offline documentation for every added driver and simple integration description.
+- Template integration files.
---
+++ doc/config_dox/nrfx_prs_dox_config.h
@@ -0,0 +1,63 @@
+/**
+ *
+ * @defgroup nrfx_prs_config Peripheral Resource Sharing module configuration
+ * @{
+ * @ingroup nrfx_prs
+ */
+/** @brief Enabling peripherals with same ID coexistence
+ *
+ *  Set to 1 to activate.
+ *
+ * @note This is an NRF_CONFIG macro.
+ */
+#define NRFX_PRS_ENABLED
+/** @brief Enables box 0 in the module.
+ *
+ *  Set to 1 to activate.
+ *
+ * @note This is an NRF_CONFIG macro.
+ */
+#define NRFX_PRS_BOX_0_ENABLED
+
+/** @brief Enables box 1 in the module.
+ *
+ *  Set to 1 to activate.
+ *
+ * @note This is an NRF_CONFIG macro.
+ */
+#define NRFX_PRS_BOX_1_ENABLED
+
+/** @brief Enables box 2 in the module.
+ *
+ *  Set to 1 to activate.
+ *
+ * @note This is an NRF_CONFIG macro.
+ */
+#define NRFX_PRS_BOX_2_ENABLED
+
+/** @brief Enables box 3 in the module.
+ *
+ *  Set to 1 to activate.
+ *
+ * @note This is an NRF_CONFIG macro.
+ */
+#define NRFX_PRS_BOX_3_ENABLED
+
+/** @brief Enables box 4 in the module.
+ *
+ *  Set to 1 to activate.
+ *
+ * @note This is an NRF_CONFIG macro.
+ */
+#define NRFX_PRS_BOX_4_ENABLED
+
+/** @brief Enables box 5 in the module.
+ *
+ *  Set to 1 to activate.
+ *
+ * @note This is an NRF_CONFIG macro.
+ */
+#define NRFX_PRS_BOX_5_ENABLED
+
+
+/** @} */
